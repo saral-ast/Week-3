702325560fe3c6b1df4461a04e5dac0d7caca6de
saral-ast <saralchauhan.ast@gmail.com>
2025-01-21 11:14:53 +0530

    Variable Commit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Starting script.js");
+console.log("Starting script.js");    // printing in console
+
+
+// alert("Hello world!") // alert
+// [1, 2].forEach(alert)   - it gives error because semicolun is not after one alert
+
+
+/* it is a multiple
+  lie cometns
+  works*/
+
+let name = "Hello world!"
+var script = "script.js"  //block scpoe issue 
+const lastName = "Doe"  //you can't change its' value
+
+{
+    let name = "Hello world!- block" //both name variable is diffrent
+    var script = "script.js-new" // here it overwrites the value and it's same for whole file 
+    // console.log(name,script,lastName) 
+}
+// console.log(name,script,lastName) //
+
