cefdf8ddd8fd98908a18f7f751a9ced06af9eeec
saral-ast <saralchauhan.ast@gmail.com>
2025-01-21 12:06:29 +0530

    type conversion

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ console.log("Starting script.js");    // printing in console
   lie cometns
   works*/
 
-alert("Hello world!") ;
-const resultPrommpt = prompt("What's Your Age",0) ;  // Prompt takes two values tile and default value
-    console.log(result) ;
+// alert("Hello world!") ;
+// const resultPrommpt = prompt("What's Your Age",0) ;  // Prompt takes two values tile and default value
+//     console.log(result) ;
 
-const result = confirm("Are you sure?") ;  // Confirm box if you click ok it returns true and if you click cancel it returns false 
-console.log(result) ;   
+// const result = confirm("Are you sure?") ;  // Confirm box if you click ok it returns true and if you click cancel it returns false 
+// console.log(result) ;   
 
 
 let name = "Hello world!"
@@ -30,16 +30,69 @@ const lastName = "Doe"  //you can't change its' value
 
 
 // Data types
-console.log(9007199254740991 + 1); // 9007199254740992  - bigInt
+// console.log(9007199254740991 + 1); // 9007199254740992  - bigInt
 
 
-let dataTypes;
-    console.log(typeof dataTypes); // undefined
-    console.log(typeof 0); // number
-    console.log(typeof 10n); // bigint
-    console.log(typeof true); // boolean
-    console.log(typeof "hello"); // string
-    console.log(typeof Symbol("id")); // symbol
-    console.log(typeof Math); // object
-    console.log(typeof null); // object
-    // use typeof for identify datatypes
+// let dataTypes;
+//     console.log(typeof dataTypes); // undefined
+//     console.log(typeof 0); // number
+//     console.log(typeof 10n); // bigint
+//     console.log(typeof true); // boolean
+//     console.log(typeof "hello"); // string
+//     console.log(typeof Symbol("id")); // symbol
+//     console.log(typeof Math); // object
+//     console.log(typeof null); // object
+//     // use typeof for identify datatypes
+
+
+let str1 = "-1234            ";
+str1 = Number(str1);
+console.log(str1); // -1234
+
+let str = "123 4";
+str = Number(str);
+console.log(str); // NaN
+
+let booLean = true;
+let booLean2 = false;
+booLean = Number(booLean);
+booLean2 = Number(booLean2);
+console.log(booLean,booLean2); // 1 0
+
+let Null = null;
+Null = Number(Null);
+console.log(Null); // 0
+
+let Undefined;
+Undefined = Number(Undefined);
+console.log(Undefined); // NaN
+
+let valueOf = 123;
+valueOf = String(valueOf);
+console.log(valueOf); // "123"
+
+let valueOf2 = true;
+valueOf2 = String(valueOf2);
+console.log(valueOf2); // "true"
+
+let valueOf3 = null;
+valueOf3 = String(valueOf3);
+console.log(valueOf3); // "null"
+
+
+
+let valueOf4 = 123;
+valueOf4 = Boolean(valueOf4);
+console.log(valueOf4); // true
+
+let valueOf5 = 0;
+valueOf5 = Boolean(valueOf5);
+console.log(valueOf5); // false
+
+let valueOf6 = "hello";
+valueOf6 = Boolean(valueOf6);
+console.log(valueOf6); // true
+
+let valueOf7 = "";
+valueOf7 = Boolean(valueOf7);
+console.log(valueOf7); // false
